8c88df79209dd982127d063de62869369f426376
Luiz Claudio <lcluizclaudio96@gmail.com>
2024-08-12 18:10:42 -0300

    fix: 🐛 prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { StudentsModule } from './students/students.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { LessonsModule } from './lessons/lessons.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, StudentsModule, TeachersModule, LessonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,8 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService extends PrismaClient implements OnModuleInit, OnModuleDestroy{
-  async onModuleInit() {
-    await this.$connect();
-  }
-  async onModuleDestroy() {
-    await this.$disconnect();
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
   }
 }
